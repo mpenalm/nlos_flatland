f0427719917e94fce5f6edc17796fa1e36cd4044
María <mpena@unizar.es>
2023-07-20 13:40:40 +0200

    some UI changes

--- src/tantalum-transient.js
+++ src/tantalum-transient.js
@@ -21,7 +21,7 @@ var Transient = function () {
     } catch (e) {
         /* Errors here are a bit more serious and shouldn't normally happen.
            Let's just dump what we have and hope the user can make sense of it */
-        this.fail("Ooops! Something unexpected happened. The error message is listed below:<br/>" +
+        this.fail("Something unexpected happened. The error message is listed below:<br/>" +
             "<pre>" + e.message + "</pre>");
         return;
     }
@@ -264,14 +264,14 @@ Transient.prototype.setupUI = function () {
     var instantSlider = null;
     changeInstantSlider();
 
-    var sigmaSlider = new tui.Slider("sigma-slider", 1, 150, true, function (sigma) {
+    var sigmaSlider = new tui.Slider("sigma-slider", 1, 200, true, function (sigma) {
         this.setLabel(`Standard deviation: ${sigma / 10} cm`);
         sigma = sigma / 1000;
         renderer.setSigma(sigma);
     })
     sigmaSlider.setValue(20);
 
-    var wlSlider = new tui.Slider("wl-slider", 1, 15, true, function (wl) {
+    var wlSlider = new tui.Slider("wl-slider", 1, 20, true, function (wl) {
         this.setLabel(`Wavelength: ${wl} cm`);
         wl = wl / 100;
         renderer.setWavelength(wl);
@@ -597,14 +597,14 @@ Transient.prototype.setupUI = function () {
     }
 
     var roughness = 0.01;
-    var roughnessSlider = new tui.Slider("roughness-slider", 1, 200, true, function (alpha) {
+    var roughnessSlider = new tui.Slider("roughness-slider", 1, 100, true, function (alpha) {
         this.setLabel(alpha / 100);
         roughness = alpha / 100;
     });
     roughnessSlider.setValue(10);
 
     var wallRoughness = 0.5;
-    var wallRoughnessSlider = new tui.Slider("rwall-roughness", 1, 200, true, function (alpha) {
+    var wallRoughnessSlider = new tui.Slider("rwall-roughness", 1, 100, true, function (alpha) {
         this.setLabel(alpha / 100);
         wallRoughness = alpha / 100;
     });
@@ -751,23 +751,30 @@ Transient.prototype.setupUI = function () {
     });
 }
 
+function isMobile() {
+    if (navigator.userAgentData != undefined) {
+        return navigator.userAgentData.mobile;
+    } else {
+        // Source: https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+        let check = false;
+        (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+    }
+}
+
 Transient.prototype.fail = function (message) {
+    var text = (isMobile()) ? "Save the link and connect from a computer, it won't run on most mobile devices" : "The demo is not working";
     var sorryP = document.createElement("p");
-    sorryP.appendChild(document.createTextNode("Sorry! :("));
+    sorryP.appendChild(document.createTextNode(text));
     sorryP.style.fontSize = "50px";
 
     var failureP = document.createElement("p");
     failureP.className = "warning-box";
     failureP.innerHTML = message;
 
-    var errorImg = document.createElement("img");
-    errorImg.title = errorImg.alt = "The Element of Failure";
-    errorImg.src = "derp.gif";
-
     var failureDiv = document.createElement("div");
     failureDiv.className = "center";
     failureDiv.appendChild(sorryP);
-    failureDiv.appendChild(errorImg);
     failureDiv.appendChild(failureP);
 
     document.getElementById("transient-content").appendChild(failureDiv);
